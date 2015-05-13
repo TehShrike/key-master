c271ce4ec66487f80e80691cc816c2a32db3ad7c
Joseph Dykstra <josephdykstra@gmail.com>
2015-05-13 09:21:55 -0500

    a few tweaks

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ module.exports = function(constructor) {
 	var map = {}
 
 	function get(key) {
-		if (typeof map[key] === 'undefined') {
-			map[key] = constructor()
+		if (typeof map[key] === 'undefined' && typeof constructor === 'function') {
+			map[key] = new constructor()
 		}
 
 		return map[key]
 	}
 
 	function remove(key) {
-		delete map[key]
+		return delete map[key]
 	}
 
 	function set(key, value) {
-		map[key] = value
+		return map[key] = value
 	}
 
 	return {
