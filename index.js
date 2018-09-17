3e8ca4e4255f5d3f5b2caa817c8ec472a76886b9
Josh Duff <me@JoshDuff.com>
2018-09-17 08:40:36 -0500

    Make it even smaller

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-module.exports = (factory, map = new Map()) => {
-
-	function get(key) {
+module.exports = (factory, map = new Map()) => ({
+	has: key => map.has(key),
+	get: key => {
 		if (!map.has(key) && typeof factory === 'function') {
 			map.set(key, factory(key))
 		}
 
 		return map.get(key)
-	}
-
-	return {
-		has: key => map.has(key),
-		get,
-		delete: key => map.delete(key),
-		set: (key, value) => map.set(key, value),
-		getUnderlyingDataStructure: () => map,
-	}
-}
+	},
+	delete: key => map.delete(key),
+	set: (key, value) => map.set(key, value),
+	getUnderlyingDataStructure: () => map,
+})
