45e1640f2c89de51efc315aad53b8633321c40a6
Martin Jurča <martin.jurca@firma.seznam.cz>
2019-06-26 13:44:31 +0200

    made the type of the underlying map implementation part of the type parameters

--- index.ts
+++ index.ts
@@ -5,22 +5,28 @@ interface IUnderlyingMap<K, V> {
 	set(key: K, value: V): this
 }
 
-interface IManagedMap<K, V, GR = V> {
+interface IManagedMap<K, V, GR, U extends IUnderlyingMap<K, V>> {
 	has(key: K): boolean
 	get(key: K): GR
 	delete(key: K): boolean
-	set(key: K, value: V): IUnderlyingMap<K, V>
-	getUnderlyingDataStructure(): IUnderlyingMap<K, V>
+	set(key: K, value: V): U
+	getUnderlyingDataStructure(): U
 }
 
 type ValueFactory<K, V> = (key: K) => V
 
-function mapFactory<K, V>(factory?: undefined, map?: IUnderlyingMap<K, V>): IManagedMap<K, V, undefined | V>
-function mapFactory<K, V>(factory: ValueFactory<K, V>, map?: IUnderlyingMap<K, V>): IManagedMap<K, V>
-function mapFactory<K, V, F extends undefined | ValueFactory<K, V> = undefined | ValueFactory<K, V>>(
-	factory?: F,
-	map: IUnderlyingMap<K, V> = new Map(),
-): IManagedMap<K, V, undefined | V> {
+function mapFactory<K, V, U extends IUnderlyingMap<K, V> = Map<K, V>>(
+	factory?: undefined,
+	map?: U,
+): IManagedMap<K, V, undefined | V, U>
+function mapFactory<K, V, U extends IUnderlyingMap<K, V> = Map<K, V>>(
+	factory: ValueFactory<K, V>,
+	map?: U,
+): IManagedMap<K, V, V, U>
+function mapFactory<K, V, U extends IUnderlyingMap<K, V> = Map<K, V>>(
+	factory?: ValueFactory<K, V>,
+	map: U = new Map<K, V>() as unknown as U,
+): IManagedMap<K, V, undefined | V, U> {
 	return {
 		has: (key: K): boolean => map.has(key),
 		get: (key: K): undefined | V => {
@@ -31,8 +37,8 @@ function mapFactory<K, V, F extends undefined | ValueFactory<K, V> = undefined |
 			return map.get(key)
 		},
 		delete: (key: K): boolean => map.delete(key),
-		set: (key: K, value: V): IUnderlyingMap<K, V> => map.set(key, value),
-		getUnderlyingDataStructure: (): IUnderlyingMap<K, V> => map,
+		set: (key: K, value: V): U => map.set(key, value),
+		getUnderlyingDataStructure: (): U => map,
 	}
 }
 
